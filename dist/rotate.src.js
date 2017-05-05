d1bcc53baaccd43050b4211354a01af67bc7fd7f
Emil Temirov <emil.temirov@litslink.com>
2017-05-05 17:01:09 +0300

    The state.direction: Fixed bug

--- dist/rotate.src.js
+++ dist/rotate.src.js
@@ -183,7 +183,7 @@
 
             this._updateAngle();
             this._applySpeed();
-            var stop = this._applyInertia();
+            this._applyInertia();
 
             const angleDiff = this.state.lastAppliedAngle - this.state._angle;
 
@@ -198,9 +198,6 @@
                 this.state.lastAppliedAngle = this.state._angle;
             }
 
-            if (stop) {
-                this.trigger(EVENT_ROTATE_STOP, this.state);
-            }
             requestAnimFrame(this._update);
         },
 
@@ -288,21 +285,18 @@
         },
 
         _applyInertia: function() {
-            var stop = false;
-
             if (this.state.inertia > 0) {
                 if (Math.abs(this.state.speed) >= this.state.minimalSpeed) {
                     this.state.speed = this.state.speed * this.state.inertia;
 
                     // Execute onStop callback if stopped
                     if (this.state.active === false && Math.abs(this.state.speed) < this.state.minimalSpeed) {
-                        stop = true;
+                        this.trigger(EVENT_ROTATE_STOP, this.state);
                     }
                 } else if (this.state.speed !== 0) {
                     this.state.speed = 0;
                 }
             }
-            return stop;
         },
 
         _blockTransition: function() {
